6d807a9eab27c77214ec1a437e4e4a4660db8849
Nehal-Sheikh <sheikhmuhammadnehal07@gmail.com>
2026-05-13 00:07:05 +0500

    vercel config

---
+++ api/index.ts
@@ -0,0 +1,36 @@
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { AppModule } from '../src/app.module';
+import helmet from 'helmet';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  
+  app.use(helmet());
+  app.enableCors({
+    origin: (origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) => {
+      if (!origin || origin.includes('vercel.app') || origin.includes('localhost')) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    credentials: true,
+  });
+
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    transform: true,
+    forbidNonWhitelisted: true,
+  }));
+
+  await app.init();
+  return app.getHttpAdapter().getInstance();
+}
+
+const handler = async (req: any, res: any) => {
+  const instance = await bootstrap();
+  instance(req, res);
+};
+
+export default handler;
